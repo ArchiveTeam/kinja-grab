60b4d3808a88430e2af27861bcc26678ab8d27a3
arkiver <arkiver@protonmail.com>
2020-11-11 04:01:54 +0100

    Version 20201111.02. Add latest get-wget-lua.sh.

--- pipeline.py
+++ pipeline.py
@@ -49,7 +49,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20201111.01'
+VERSION = '20201111.02'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'kinja'
 TRACKER_HOST = 'tracker.archiveteam.org'
